0d766c0249419e3d78f0add15a48bd7ebc73254a
Gustavo <77560533+ofoxsmith@users.noreply.github.com>
2022-10-20 12:35:23 +1100

    move ammo manager to canada

--- canada_actions.lua
+++ canada_actions.lua
@@ -1,28 +1,68 @@
 dofile_once("CANADA_PATHcanada_util.lua")
 local CanadaCard = {}
+
 function CanadaCard:New(id)
     local o = {}
+    local prop_types = {
+        ammo_system_recharge_time = "int",
+        ammo_system_capacity = "int",
+        ammo_system_remaining = "int",
+        ammo_system_recharge_while_shooting = "bool",
+        ammo_system_locked = "bool"
+    }
     o.cardId = id
     setmetatable(o, {
-        __index = function(t,k)
-            if k == "ammo" then return self:GetAmmo(t.card) end
+        __index = function(t, k)
+            if k == "ammo" then
+                return self:GetAmmo(o.cardId)
+            end
+            if prop_types[k] ~= nil then
+                local vsc = EntityGetComponentIncludingDisabled(id, "VariableStorageComponent");
+                local comp
+                for _ = 1, #vsc do
+                    local v = vsc[_]
+                    if ComponentGetValue2(v, "name") == k then
+                        comp = v
+                    end
+                end
+                if vsc ~= nil then
+                    return ComponentGetValue2(comp, ("value_%s"):format(prop_types[k]))
+                end
+                return nil
+            end
             return self[k]
         end,
         __newindex = function(t, k, v)
-            if k == "ammo" then return self:SetAmmo(t.card, v) end
-        end    
+            if k == "ammo" then
+                return self:SetAmmo(o.cardId, v)
+            end
+            if prop_types[k] ~= nil then
+                local vsc = EntityGetComponentIncludingDisabled(id, "VariableStorageComponent");
+                local comp
+                for _ = 1, #vsc do
+                    local val = vsc[_]
+                    if ComponentGetValue2(v, "name") == k then
+                        comp = val
+                    end
+                end
+                if vsc ~= nil then
+                    ComponentSetValue2(comp, ("value_%s"):format(prop_types[k]), v)
+                end
+            end
+        end,
     })
     function self:New()
         return nil
-    end;
+    end
     return o
 end
 
-function CreateCard(id)
-    return CanadaCard:New(id)
-end
-
-function IsActionUnlimited( entity , action )
+setmetatable(CanadaCard, {
+    __call = function(id)
+        return CanadaCard:New(id)
+    end
+})
+function IsActionUnlimited(entity, action)
     local unlimited = false
     if action.permanently_attached then
         unlimited = true
@@ -30,8 +70,8 @@ function IsActionUnlimited( entity , action )
     return unlimited
 end
 
-function CurrentCard( entity )
-    local wand = GetWand( entity )
+function CurrentCard(entity)
+    local wand = GetWand(entity)
     local cards = GetSpells(wand)
     local me = hand[#hand]
     local mycard = cards[me.deck_index + 1]
@@ -39,23 +79,26 @@ function CurrentCard( entity )
 end
 
 function CanadaCard:GetAmmo()
-    if IsActionUnlimited( GetUpdatedEntityID() ) then
+    if IsActionUnlimited(GetUpdatedEntityID()) then
         return 1
     else
-        local vsc = EntityGetFirstComponentIncludingDisabled( self.cardId, "VariableStorageComponent", "ammo_system_remaining" );
+        local vsc = EntityGetFirstComponentIncludingDisabled(self.cardId, "VariableStorageComponent",
+            "ammo_system_remaining");
         if vsc ~= nil then
-            return ComponentGetValue2( vsc, "value_int" )
+            return ComponentGetValue2(vsc, "value_int")
         end
     end
 end
 
 function CanadaCard:SetAmmo(count)
-    if IsActionUnlimited( GetUpdatedEntityID() ) then
+    if IsActionUnlimited(GetUpdatedEntityID()) then
         return
     else
-        local vsc = EntityGetFirstComponentIncludingDisabled( self.cardId, "VariableStorageComponent", "ammo_system_remaining" );
+        local vsc = EntityGetFirstComponentIncludingDisabled(self.cardId, "VariableStorageComponent",
+            "ammo_system_remaining");
         if vsc ~= nil then
-            ComponentSetValue2( vsc, "value_int", count )
+            ComponentSetValue2(vsc, "value_int", count)
         end
     end
 end
+
