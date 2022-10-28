27812d6b59807861340f29f3a30938bcd5314291
Gustavo <77560533+ofoxsmith@users.noreply.github.com>
2022-10-28 13:41:48 +1100

    e

--- canada_display.lua
+++ canada_display.lua
@@ -1,15 +1,19 @@
-dofile_once("CANADA_PATHcanada_lib.lua")
---- @module "canada_lib"
 
+dofile_once("CANADA_PATHcanada_lib.lua")
+
+gui = gui or GuiCreate()
+GuiStartFrame(gui)
 local entity_id = GetUpdatedEntityID()
 local canada_card = CanadaCard(entity_id)
 local shooter = EntityGetParent(EntityGetParent(entity_id))
-
-local todisplay = canada_card.remaining
-
 local iter = tonumber(GlobalsGetValue("canada_lib_display_iter", "0"))
 local iter_tl = tonumber(GlobalsGetValue("canada_lib_display_iter_tl", "0"))
 local pos_dist = 2 + iter * 5
+_id = 0
+local getID = function ()
+    _id = _id + 1
+    return _id
+end
 
 local angle = { min = 10, max = 170 }
 local angle_delta = 160 / canada_card.capacity
@@ -27,11 +31,8 @@ if controlscomp ~= nil then
         GlobalsSetValue("canada_lib_display_iter", tostring(iter + 1))
     end
     while (todisplay > 0) do
-        GameCreateSpriteForXFrames("CANADA_PATHcanada_gfx/ammo_fill.png",
-            mouse_x + (pos_dist * math.cos(math.rad(angle.min + (angle_delta * todisplay)))),
-            mouse_y + (pos_dist * math.sin(math.rad(angle.min + (angle_delta * todisplay)))),
-            true, 0, 0, 1, true)
-
+        GuiImage(gui, getID(), mouse_x + (pos_dist * math.cos(math.rad(angle.min + (angle_delta * todisplay)))),
+        mouse_y + (pos_dist * math.sin(math.rad(angle.min + (angle_delta * todisplay)))), "CANADA_PATHcanada_gfx/ammo_fill.png", 1, 1, 0, 0)
         todisplay = todisplay - 1
     end
 end
--- canada_utils.lua
+++ canada_utils.lua
@@ -9,8 +9,7 @@ dofile_once("CANADA_PATHcanada_lib.lua")
 --- @param reload_on_empty boolean Wether to use the reload on empty system, where the card only reloads when all ammo is expended. 
 --- @param enabled_in_inventory boolean? If the canada scripts should be enabled when the wand is not being held. Default is false
 --- @param controller_script_path? string Path to script used to control ammo, you likely do not want to mess with this unless you absolutely know what you're doing
---- @param display_script_path? string Path to script used when displaying ammo count
-function RegisterCanadaAction(card_entity_path, recharge_time, capacity, initial_ammo, recharge_while_shooting, reload_on_empty, enabled_in_inventory, controller_script_path, display_script_path)
+function RegisterCanadaAction(card_entity_path, recharge_time, capacity, initial_ammo, recharge_while_shooting, reload_on_empty, enabled_in_inventory, controller_script_path)
     -- If the world has been initialised, then ModEntityFileAddComponent won't work
     if ModTextFileSetContent == nil then
         error("RegisterCanadaAction: Actions can only be registered before world init", 2)
@@ -20,7 +19,6 @@ function RegisterCanadaAction(card_entity_path, recharge_time, capacity, initial
     end
     --- Default scripts
     enabled_in_inventory = enabled_in_inventory or false
-    display_script_path    = display_script_path or "CANADA_PATHcanada_display.lua"
     controller_script_path = controller_script_path or "CANADA_PATHcanada_controller.lua"
 
     --- Add components to card file contents
@@ -29,7 +27,6 @@ function RegisterCanadaAction(card_entity_path, recharge_time, capacity, initial
     ModEntityFileAddComponent(card_entity_path, GenerateVSC("ammo_system_remaining", initial_ammo))
     ModEntityFileAddComponent(card_entity_path, GenerateVSC("ammo_system_recharge_while_shooting", recharge_while_shooting))
     ModEntityFileAddComponent(card_entity_path, GenerateVSC("ammo_system_reload_on_empty", reload_on_empty))
-    ModEntityFileAddComponent(card_entity_path, ('<LuaComponent _tags="enabled_in_hand" execute_every_n_frame="1" script_source_file="%s" ></LuaComponent>'):format(display_script_path))
     ModEntityFileAddComponent(card_entity_path, ('<LuaComponent _tags="%s" execute_every_n_frame="1" script_source_file="%s" ></LuaComponent>'):format((enabled_in_inventory and "enabled_in_inventory" or "enabled_in_hand"), controller_script_path))
 end
 
@@ -38,6 +35,20 @@ function CanadaGuiPostUpdate()
     GlobalsSetValue("canada_lib_display_iter_tl", "0")
 end
 
+local _id = 0
+function id()
+    _id = _id + 1
+    return _id
+end
+function CanadaDisplay()
+    CanadaGuiPostUpdate()
+    gui = gui or GuiCreate()
+    local screenWidth, screenHeight = GuiGetScreenDimensions(gui)
+    local cComp = EntityGetFirstComponent(EntityGetWithTag("player_unit")[1], "ControlsComponent")
+    if cComp == nil then return end
+    local mouse_raw_x, mouse_raw_y = ComponentGetValue2(cComp, "mMousePositionRaw")
+    sx, sy = mouse_raw_x * screenWidth / 1280, mouse_raw_y * screenHeight / 720
+end
 --- ### Print a file path's contents
 --- ***
 --- @param file_path string The file path to print
